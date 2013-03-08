c17e4467283779f6c9d8e9786a74a6d201961d39
Tadayoshi Sato <tadayosi@gmail.com>
2013-03-08 14:56:53 +0900

    Added helloworld quickstart. Fixed #1

---
+++ helloworld/src/main/java/org/switchyard/quickstarts/helloworld/MyJMSListenerAction.java
@@ -0,0 +1,12 @@
+package org.switchyard.quickstarts.helloworld;
+
+public class MyJMSListenerAction {
+    
+    public String displayMessage(String message) throws Exception {
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        System.out.println("Body: " + message);
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        return message;
+    }
+    
+}
