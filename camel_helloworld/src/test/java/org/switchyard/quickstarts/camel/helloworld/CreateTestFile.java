73c2370d8dbc6b2940459d79954b95c5bcc62c91
Hisao Furuichi <hfuruichi.copal@gmail.com>
2013-07-17 19:46:02 +0900

    Added camel_helloworld quickstart

---
+++ camel_helloworld/src/test/java/org/switchyard/quickstarts/camel/helloworld/CreateTestFile.java
@@ -0,0 +1,24 @@
+package org.switchyard.quickstarts.camel.helloworld;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class CreateTestFile {
+    
+    public static void main(String args[]) throws Exception {
+        String inputDirectory = args[0];
+        String fileName = args[1];
+        String fileContents = args[2];
+        File x = new File(inputDirectory + "/" + fileName);
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(x));
+            out.write(fileContents.toCharArray());
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Error while writing the file: " + inputDirectory + "/" + fileName);
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
